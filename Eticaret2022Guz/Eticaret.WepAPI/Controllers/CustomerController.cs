b92f61dff895e6ed8543e99e72284e919575b9d3
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-04 18:57:04 +0300

    WebAPI de CustomerController oluşturuldu

---
+++ Eticaret2022Guz/Eticaret.WepAPI/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using ETicaret.BussinessLayer.Abstract;
+using ETicaret.BussinessLayer.Concrete;
+using ETicaret.EntityLayer.Concretes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Eticaret.WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+       private ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<List<Customer>> GetList()
+        {
+          return  await _customerService.GetListAsync(x => x.CustomerStatus == true);
+        }
+
+    }
+}
