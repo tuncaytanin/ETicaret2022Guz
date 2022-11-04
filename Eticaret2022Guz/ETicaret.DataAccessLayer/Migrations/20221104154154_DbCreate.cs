28a89f65de52926b64c82e34979644ef954f7a48
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-04 19:01:30 +0300

    First DB Create migration oluşturuldu

---
+++ Eticaret2022Guz/ETicaret.DataAccessLayer/Migrations/20221104154154_DbCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETicaret.DataAccessLayer.Migrations
+{
+    public partial class DbCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CustomerLastName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    CustomerEmail = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    CustomerUserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BirthDay = table.Column<DateTime>(type: "date", nullable: false),
+                    datetime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()"),
+                    CustomerStatus = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    varchar = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "BirthDay", "datetime", "CustomerEmail", "CustomerLastName", "CustomerName", "CustomerStatus", "CustomerUserName", "varchar" },
+                values: new object[] { 1, new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 11, 4, 18, 41, 53, 945, DateTimeKind.Local).AddTicks(5961), "tt@gmail.com", "Tanin", "Tuncay", true, "tt", "+905327411235" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
