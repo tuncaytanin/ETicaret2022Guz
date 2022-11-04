e44b81137b0ec2f9fa447bb78d49fc9f5d1d5dc6
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-04 18:57:57 +0300

    I CustomerService oluşturuldu

---
+++ Eticaret2022Guz/ETicaret.BussinessLayer/Abstract/ICustomerService.cs
@@ -0,0 +1,23 @@
+﻿using ETicaret.EntityLayer.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.BussinessLayer.Abstract
+{
+    public interface ICustomerService
+    {
+        Task<Customer> AddAsync(Customer customer);
+        Task<Customer> UpdateAsync(Customer customer);
+        Task<bool> DeleteByIdAsync(int id);
+
+        Task<List<Customer>> GetListAsync(Expression<Func<Customer, bool>> filter = null);
+
+        Task<Customer> GetAsync(Expression<Func<Customer, bool>> filter);
+
+        Task<Customer> GetModelByIdAsync(int id);
+    }
+}
