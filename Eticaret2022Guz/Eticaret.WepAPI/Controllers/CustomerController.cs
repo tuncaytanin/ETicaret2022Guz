eec6751f36ddf7d4d6f945aeb61543b07185439a
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-11 18:22:14 +0300

    Custoer Api güncellendi.

--- Eticaret2022Guz/Eticaret.WepAPI/Controllers/CustomerController.cs
+++ Eticaret2022Guz/Eticaret.WepAPI/Controllers/CustomerController.cs
@@ -20,9 +20,56 @@ namespace Eticaret.WepAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<List<Customer>> GetList()
+        [Route("[action]")]
+        public async Task<IActionResult> GetList()
         {
-          return  await _customerService.GetListAsync(x => x.CustomerStatus == true);
+          var result=  await _customerService.GetListAsync(x => x.CustomerStatus == true);
+            if (result is not null)
+                return Ok(result);
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _customerService.GetModelByIdAsync(id);
+            if (result is not null)
+                return Ok(result);
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Add([FromBody] Customer customer)
+        {
+            var result = await _customerService.AddAsync(customer);
+            if (result is not null)
+                return Ok(customer);
+            return BadRequest(result);
+        }
+
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> Update([FromBody] Customer customer)
+        {
+            var result = await _customerService.UpdateAsync(customer);
+            if (result is not null)
+                return Ok(result);
+            
+            return BadRequest(result);
+        }
+
+        [HttpDelete]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _customerService.DeleteByIdAsync(id);
+            if (result)
+                return Ok(result);
+            
+            return BadRequest(result);
         }
 
     }
