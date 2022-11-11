51f270f89eab4198975bbe792607603f351f1108
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-11 18:20:32 +0300

    Müşteri Yönetim Paneli Oluşturuldu

---
+++ Eticaret2022Guz/Eticaret.Desktop/FormCustomer.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Eticaret.Desktop
+{
+    partial class FormCustomer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKAdi = new System.Windows.Forms.Label();
+            this.txtCustomerUserName = new System.Windows.Forms.TextBox();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustomerSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpBirthDay = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.dtgvCustomers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKAdi
+            // 
+            this.lblKAdi.AutoSize = true;
+            this.lblKAdi.Location = new System.Drawing.Point(97, 19);
+            this.lblKAdi.Name = "lblKAdi";
+            this.lblKAdi.Size = new System.Drawing.Size(92, 20);
+            this.lblKAdi.TabIndex = 0;
+            this.lblKAdi.Text = "Kullanıcı Adı";
+            // 
+            // txtCustomerUserName
+            // 
+            this.txtCustomerUserName.Location = new System.Drawing.Point(218, 16);
+            this.txtCustomerUserName.Name = "txtCustomerUserName";
+            this.txtCustomerUserName.Size = new System.Drawing.Size(252, 27);
+            this.txtCustomerUserName.TabIndex = 1;
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Location = new System.Drawing.Point(12, 12);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.ReadOnly = true;
+            this.txtCustomerId.Size = new System.Drawing.Size(40, 27);
+            this.txtCustomerId.TabIndex = 1;
+            this.txtCustomerId.Text = "0";
+            this.txtCustomerId.TextChanged += new System.EventHandler(this.txtCustomerId_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Adı";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(218, 66);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(252, 27);
+            this.txtCustomerName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Müşteri Soyadı";
+            // 
+            // txtCustomerSurname
+            // 
+            this.txtCustomerSurname.Location = new System.Drawing.Point(218, 121);
+            this.txtCustomerSurname.Name = "txtCustomerSurname";
+            this.txtCustomerSurname.Size = new System.Drawing.Size(252, 27);
+            this.txtCustomerSurname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(634, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Telefon";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(755, 121);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(252, 27);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(627, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Doğum Tarihi";
+            // 
+            // dtpBirthDay
+            // 
+            this.dtpBirthDay.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirthDay.Location = new System.Drawing.Point(755, 16);
+            this.dtpBirthDay.Name = "dtpBirthDay";
+            this.dtpBirthDay.Size = new System.Drawing.Size(252, 27);
+            this.dtpBirthDay.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(634, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(755, 66);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(252, 27);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnEkle.FlatAppearance.BorderSize = 0;
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEkle.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEkle.ForeColor = System.Drawing.Color.White;
+            this.btnEkle.Location = new System.Drawing.Point(300, 186);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(120, 54);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Red;
+            this.btnSil.FlatAppearance.BorderSize = 0;
+            this.btnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSil.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSil.ForeColor = System.Drawing.Color.White;
+            this.btnSil.Location = new System.Drawing.Point(432, 186);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(120, 54);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnGuncelle.FlatAppearance.BorderSize = 0;
+            this.btnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuncelle.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnGuncelle.ForeColor = System.Drawing.Color.White;
+            this.btnGuncelle.Location = new System.Drawing.Point(567, 186);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(120, 54);
+            this.btnGuncelle.TabIndex = 3;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.Pink;
+            this.btnTemizle.FlatAppearance.BorderSize = 0;
+            this.btnTemizle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTemizle.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTemizle.ForeColor = System.Drawing.Color.White;
+            this.btnTemizle.Location = new System.Drawing.Point(703, 186);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(120, 54);
+            this.btnTemizle.TabIndex = 3;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // dtgvCustomers
+            // 
+            this.dtgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvCustomers.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dtgvCustomers.Location = new System.Drawing.Point(0, 264);
+            this.dtgvCustomers.Name = "dtgvCustomers";
+            this.dtgvCustomers.RowHeadersWidth = 51;
+            this.dtgvCustomers.RowTemplate.Height = 29;
+            this.dtgvCustomers.Size = new System.Drawing.Size(1179, 513);
+            this.dtgvCustomers.TabIndex = 4;
+            this.dtgvCustomers.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dtgvCustomers_RowHeaderMouseDoubleClick);
+            // 
+            // FormCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 777);
+            this.Controls.Add(this.dtgvCustomers);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dtpBirthDay);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCustomerSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCustomerUserName);
+            this.Controls.Add(this.lblKAdi);
+            this.Name = "FormCustomer";
+            this.Text = "Müşteri Yönetimi";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKAdi;
+        private System.Windows.Forms.TextBox txtCustomerUserName;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustomerSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpBirthDay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.DataGridView dtgvCustomers;
+    }
+}
---
+++ Eticaret2022Guz/Eticaret.Desktop/FormCustomer.cs
@@ -0,0 +1,150 @@
+﻿using ETicaret.EntityLayer.Concretes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eticaret.Desktop
+{
+    public partial class FormCustomer : Form
+    {
+        private string url = "https://localhost:44374/api/";
+
+        public FormCustomer()
+        {
+            InitializeComponent();
+            Load += FormCustomer_Load;
+        }
+
+        private async void FormCustomer_Load(object? sender, EventArgs e)
+        {
+            await UyeleriListele();
+        }
+
+        private async Task UyeleriListele()
+        {
+            using (HttpClient httpClient  = new HttpClient())
+            {
+                var customers = await httpClient.GetFromJsonAsync<List<Customer>>(new Uri(url+ "Customer/GetList"));
+                dtgvCustomers.DataSource = customers;
+            }
+        }
+
+        private async void btnEkle_Click(object sender, EventArgs e)
+        {
+
+            Customer customer = new Customer()
+            {
+                BirthDay = dtpBirthDay.Value,
+                CreateDate = DateTime.Now,
+                CustomerEmail = txtEmail.Text,
+                CustomerLastName = txtCustomerSurname.Text,
+                CustomerStatus = true,
+                CustomerName = txtCustomerName.Text,
+                CustomerUserName = txtCustomerUserName.Text,
+                PhoneNumber = txtPhone.Text
+            };
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = await httpClient.PostAsJsonAsync(url + "Customer/Add/", customer);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    await UyeleriListele();
+                    FormClear();
+                }
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            FormClear();
+        }
+
+        private void FormClear()
+        {
+            txtCustomerId.Text = "0";
+            txtCustomerName.Text = string.Empty;
+            dtpBirthDay.Value = DateTime.Now;
+            txtCustomerSurname.Text = string.Empty;
+            txtCustomerUserName.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            
+        }
+
+        private void dtgvCustomers_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            txtCustomerId.Text = dtgvCustomers.CurrentRow.Cells[0].Value.ToString();
+            txtCustomerName.Text = dtgvCustomers.CurrentRow.Cells["CustomerName"].Value.ToString();
+
+            txtCustomerSurname.Text = dtgvCustomers.CurrentRow.Cells["CustomerLastName"].Value.ToString();
+            txtCustomerUserName.Text = dtgvCustomers.CurrentRow.Cells["CustomerUserName"].Value.ToString();
+            txtEmail.Text = dtgvCustomers.CurrentRow.Cells["CustomerEmail"].Value.ToString();
+            txtPhone.Text = dtgvCustomers.CurrentRow.Cells["PhoneNumber"].Value.ToString();
+            dtpBirthDay.Value = Convert.ToDateTime(dtgvCustomers.CurrentRow.Cells["BirthDay"].Value.ToString());
+        }
+
+        private void txtCustomerId_TextChanged(object sender, EventArgs e)
+        {
+            ButonlarKontrolu();
+        }
+
+        private void ButonlarKontrolu()
+        {
+            bool aktifEt = false;
+            if (!string.IsNullOrEmpty(txtCustomerId.Text))
+            {
+                if (int.Parse(txtCustomerId.Text) > 0 )
+                {
+                    aktifEt = true;
+
+                }
+            }
+            btnSil.Enabled = aktifEt;
+            btnGuncelle.Enabled = aktifEt;
+        }
+
+        private async void btnSil_Click(object sender, EventArgs e)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                await httpClient.DeleteAsync(url + "Customer/Delete/" + txtCustomerId.Text);
+                await UyeleriListele();
+            }
+        }
+
+        private async void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer()
+            {
+                BirthDay = dtpBirthDay.Value,
+                CreateDate = DateTime.Now,
+                CustomerEmail = txtEmail.Text,
+                CustomerLastName = txtCustomerSurname.Text,
+                CustomerStatus = true,
+                CustomerName = txtCustomerName.Text,
+                CustomerUserName = txtCustomerUserName.Text,
+                PhoneNumber = txtPhone.Text,
+                CustomerId = int.Parse(txtCustomerId.Text)
+            };
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = await httpClient.PutAsJsonAsync(url + "Customer/Update/", customer);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    await UyeleriListele();
+                    FormClear();
+                }
+            }
+        }
+    }
+}
