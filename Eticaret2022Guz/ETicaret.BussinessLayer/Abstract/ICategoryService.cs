442431c449bcdc5185ff64adb78d05145d50aed8
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-25 18:19:33 +0300

    ICategoryService oluşturuldu

---
+++ Eticaret2022Guz/ETicaret.BussinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,23 @@
+﻿using ETicaret.EntityLayer.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ETicaret.BussinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<Category> AddAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<bool> DeleteByIdAsync(int id);
+
+        Task<List<Category>> GetListAsync(Expression<Func<Category, bool>> filter = null);
+
+        Task<Category> GetAsync(Expression<Func<Category, bool>> filter);
+
+        Task<Category> GetModelByIdAsync(int id);
+
+        Task<List<Category>> Get3CategoryAsync();
+    }
+}
